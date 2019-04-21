781b6b3f1e54ba6be9b95bf1bc5099480cb15d08
Laz <lazdisco@gmail.com>
2019-04-21 19:49:26 +0100

    Finished the rest of the blacklists, just need implementation

--- ConfigHandler.cs
+++ ConfigHandler.cs
@@ -471,6 +471,43 @@ namespace Raven
                             return InvalidOption(channel);
                     }
                 }
+
+                case MessageBox.GeneralConfigureBlacklistedRoles:
+                {
+                    switch (option)
+                    {
+                        case MessageBox.BlacklistAddTo:
+                            return BlacklistAddRole(guild, userId, channel, args);
+
+                        case MessageBox.BlacklistRemoveFrom:
+                            return BlacklistRemoveRole(guild, userId, channel, args);
+
+                        case MessageBox.BlacklistDisplay:
+                            return BlacklistDisplayRoles(guild, userId, channel, args);
+
+                        default:
+                            return InvalidOption(channel);
+                    }
+                }
+
+                case MessageBox.GeneralConfigureBlacklistedUsers:
+                {
+                    switch (option)
+                    {
+                        case MessageBox.BlacklistAddTo:
+                            return BlacklistAddUser(guild, userId, channel, args);
+
+                        case MessageBox.BlacklistRemoveFrom:
+                            return BlacklistRemoveUser(guild, userId, channel, args);
+
+                        case MessageBox.BlacklistDisplay:
+                            return BlacklistDisplayUsers(guild, userId, channel, args);
+
+                        default:
+                            return InvalidOption(channel);
+                    }
+                }
+                
                 default:
                     guild.UserConfiguration.Remove(userId);
                     guild.Save();
@@ -774,7 +811,7 @@ namespace Raven
             if (args.ElementAtOrDefault(1) is null) // If they didn't provide a channel name
                 return channel.SendMessageAsync(GetMissingParam("ChannelId", typeof(ulong)));
 
-            if (!uint.TryParse(args.ElementAtOrDefault(1), out uint id))
+            if (!ulong.TryParse(args.ElementAtOrDefault(1), out ulong id))
                 return channel.SendMessageAsync(ParamWrongFormat("ChannelId", typeof(ulong)));
 
             if (guild.GuildSettings.BlacklistedChannels.Any(x => x == id))
@@ -797,8 +834,7 @@ namespace Raven
 
             foreach (ulong i in guild.GuildSettings.BlacklistedChannels)
             {
-                var tempChannel = channel.Guild.Channels.FirstOrDefault(x => x.Name == string.Join('-', args.Skip(1)).ToLower());
-                tempChannel = tempChannel ?? channel.Guild.Channels.FirstOrDefault(x => x.Name.Contains(string.Join('-', args.Skip(1)).ToLower()));
+                var tempChannel = channel.Guild.Channels.FirstOrDefault(x => x.Id == i);
 
                 if (tempChannel is null)
                     channels += $"#INVALID-CHANNEL ({i})\n";
@@ -811,6 +847,130 @@ namespace Raven
                                                 + channels.Substring(0, 1800));
             else
                 return channel.SendMessageAsync("```cs\n" + channels + "\n```");
+        }
+
+        private static Task<RestUserMessage> BlacklistAddRole(RavenGuild guild, ulong userId, SocketTextChannel channel, string[] args)
+        {
+            if (args.ElementAtOrDefault(1) is null) // If they didn't provide a role name
+                return channel.SendMessageAsync(GetMissingParam("RoleName", typeof(string)));
+
+            var tempRole = channel.Guild.Roles.FirstOrDefault(x => x.Name == string.Join(' ', args.Skip(1)));
+            tempRole = tempRole ?? channel.Guild.Roles.FirstOrDefault(x => x.Name.Contains(string.Join(' ', args.Skip(1))));
+
+            if (tempRole is null) // If we found no matching channels
+                return channel.SendMessageAsync("The specified role could not be found. Please try again.");
+
+            guild.GuildSettings.BlacklistedRoles.Add(tempRole.Id);
+            guild.Save();
+            return SelectSubMenu(guild, userId, channel, MessageBox.GeneralConfigureBlacklistedRoles);
+        }
+
+        private static Task<RestUserMessage> BlacklistRemoveRole(RavenGuild guild, ulong userId, SocketTextChannel channel, string[] args)
+        {
+            if (args.ElementAtOrDefault(1) is null) // If they didn't provide a channel name
+                return channel.SendMessageAsync(GetMissingParam("RoleId", typeof(ulong)));
+
+            if (!ulong.TryParse(args.ElementAtOrDefault(1), out ulong id))
+                return channel.SendMessageAsync(ParamWrongFormat("RoleId", typeof(ulong)));
+
+            if (guild.GuildSettings.BlacklistedRoles.Any(x => x == id))
+            {
+                guild.GuildSettings.BlacklistedRoles.Remove(id);
+                guild.Save();
+                return SelectSubMenu(guild, userId, channel, MessageBox.GeneralConfigureBlacklistedRoles);
+            }
+
+            else
+                return channel.SendMessageAsync("The specified role was not blacklisted.");
+        }
+
+        private static Task<RestUserMessage> BlacklistDisplayRoles(RavenGuild guild, ulong userId, SocketTextChannel channel, string[] args)
+        {
+            if (guild.GuildSettings.BlacklistedRoles.Count == 0)
+                return channel.SendMessageAsync("There are currently no blacklisted roles.");
+
+            string roles = "Blacklisted Roles:\n";
+
+            foreach (ulong i in guild.GuildSettings.BlacklistedRoles)
+            {
+                var tempRole = channel.Guild.Roles.FirstOrDefault(x => x.Id == i);
+
+                if (tempRole is null)
+                    roles += $"DELETED-ROLE ({i})\n";
+                else
+                    roles += $"{tempRole.Name} ({i})\n";
+            }
+
+            if (roles.Length > 1900)
+                return channel.SendMessageAsync("Too many roles to list. Cutting off after 1800 characters.\n"
+                                                + roles.Substring(0, 1800));
+            else
+                return channel.SendMessageAsync("```cs\n" + roles + "\n```");
+        }
+
+        private static Task<RestUserMessage> BlacklistAddUser(RavenGuild guild, ulong userId, SocketTextChannel channel, string[] args)
+        {
+            if (args.ElementAtOrDefault(1) is null)
+                return channel.SendMessageAsync(GetMissingParam("Username", typeof(string)));
+
+            string name = string.Join(' ', args.Skip(1)).ToLower();
+
+            var user = (channel.Guild.Users.FirstOrDefault(x => (x.Username + "#" + x.DiscriminatorValue).Contains(name)) ??
+                        channel.Guild.Users.FirstOrDefault(x => x.Nickname.Contains(name))) ??
+                        channel.Guild.Users.FirstOrDefault(x => x.Id.ToString() == name);
+
+            if (user is null)
+                return channel.SendMessageAsync("Could not find a matching user.");
+
+            guild.GuildSettings.BlacklistedChannels.Add(user.Id);
+            guild.Save();
+            return SelectSubMenu(guild, userId, channel, MessageBox.GeneralConfigureBlacklistedUsers);
+        }
+
+        private static Task<RestUserMessage> BlacklistRemoveUser(RavenGuild guild, ulong userId, SocketTextChannel channel, string[] args)
+        {
+            if (args.ElementAtOrDefault(1) is null)
+                return channel.SendMessageAsync(GetMissingParam("UserId", typeof(ulong)));
+
+            if (!ulong.TryParse(args.ElementAtOrDefault(1), out ulong id))
+                return channel.SendMessageAsync(ParamWrongFormat("UserId", typeof(ulong)));
+
+            if (guild.GuildSettings.BlacklistedUsers.Any(x => x == id))
+            {
+                guild.GuildSettings.BlacklistedUsers.Remove(id);
+                guild.Save();
+                return SelectSubMenu(guild, userId, channel, MessageBox.GeneralConfigureBlacklistedUsers);
+            }
+
+            else
+                return channel.SendMessageAsync("The specified user was not blacklisted.");
+        }
+
+        private static Task<RestUserMessage> BlacklistDisplayUsers(RavenGuild guild, ulong userId, SocketTextChannel channel, string[] args)
+        {
+            if (guild.GuildSettings.BlacklistedChannels.Count == 0)
+                return channel.SendMessageAsync("There are currently no blacklisted users.");
+
+            string users = "Blacklisted Users:\n";
+
+            foreach (ulong i in guild.GuildSettings.BlacklistedUsers)
+            {
+                var user = channel.Guild.Users.FirstOrDefault(x => x.Id == i);
+                if (user is null)
+                {
+                    RavenUser deletedRavenUser = guild.Users.FirstOrDefault(x => x.UserId == i);
+                    string oldUsername = deletedRavenUser == null ? "Unknown" : deletedRavenUser.Username + "#" + deletedRavenUser.Discriminator;
+                    users += $"USER-LEFT (ID: {i} - Old Name: {oldUsername})\n";
+                }
+                else
+                    users += $"{user.Username} ({i})\n";
+            }
+
+            if (users.Length > 1900)
+                return channel.SendMessageAsync("Too many users to list. Cutting off after 1800 characters.\n"
+                                                + users.Substring(0, 1800));
+            else
+                return channel.SendMessageAsync("```cs\n" + users + "\n```");
         }
     }
 }
