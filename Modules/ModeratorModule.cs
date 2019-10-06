dae013cbd14ab633949e5d18d4a35c1c4acc9adb
LazDisco <lazdisco@gmail.com>
2019-10-06 14:17:17 +0100

    Added purge/message prune command

--- Modules/ModeratorModule.cs
+++ Modules/ModeratorModule.cs
@@ -1,4 +1,6 @@
-﻿using Discord;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
 using System.Threading.Tasks;
@@ -38,6 +40,47 @@ namespace Raven.Modules
 
             await ReplyAsync($"Bye, {user.Nickname ?? user.Username}. You probably wont be missed.");
             await user.KickAsync();
+        }
+
+        [Command("purge")]
+        [Alias("prune")]
+        [Summary("Delete the specified number of messages.")]
+        [Remarks("Capped at 100 messages per purge. Mentioning people will delete only their messages. Does not delete messages older than 14 days.")]
+        [RequireUserPermission(GuildPermission.ManageMessages)]
+        [RequireBotPermission(GuildPermission.ManageMessages, Group = "Permission")]
+        [RequireBotPermission(ChannelPermission.ManageMessages, Group = "Permission")]
+        [CheckBlacklistedCommand]
+        public async Task Purge(uint count, [Remainder]string args = "") // Dummy args param
+        {
+            if (count is 0)
+            {
+                await ReplyAsync("You have to delete a positive number of messages. Come on, you knew that.");
+                return;
+            }
+
+            else if (count > 100)
+                count = 100;
+
+            if (Context.Message.MentionedUsers.Count is 0)
+            {
+                IEnumerable<IMessage> messages = await Context.Channel.GetMessagesAsync(Context.Message, Direction.Before, (int)count).FlattenAsync();
+                if (messages.Count() is 0)
+                {
+                    await ReplyAsync("Channel is empty.");
+                    return;
+                }
+
+                await (Context.Channel as ITextChannel).DeleteMessagesAsync(messages);
+                await ReplyAsync("Done.");
+            }
+
+            else
+            {
+                IEnumerable<IMessage> messages = await Context.Channel.GetMessagesAsync(Context.Message, Direction.Before, (int)count).FlattenAsync();
+                IEnumerable<IMessage> filtered = messages.Where(x => Context.Message.MentionedUsers.Any(y => y.Id == x.Author.Id));
+                await (Context.Channel as ITextChannel).DeleteMessagesAsync(filtered);
+                await ReplyAsync($"Searched the last {count} messages and deleted all from the mentioned users.");
+            }
         }
     }
 }
